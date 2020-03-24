a4c4e7885a1e713e82e88aa320855fd27b62d6c6
Nattapong <62310291@go.buu.ac.th>
2020-03-24 10:42:53 +0700

    final3

--- index.php
+++ index.php
@@ -5,8 +5,5 @@
 <?php echo "รหัสนิสิต-62310291"; ?><br>
 <?php echo "ชื่อ-สกุล นายณัฐพงค์ หงษาวงค์"; ?><br>
 <?php echo "https://youtu.be/bNVPoFTEj6c"; ?><br>
-<iframe width="500" height="500"
-src="https://youtu.be/bNVPoFTEj6c">
-</iframe>
-</BODY>
-</HTML>
+
+</HTML><iframe width="560" height="315" src="https://www.youtube.com/embed/bNVPoFTEj6c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
